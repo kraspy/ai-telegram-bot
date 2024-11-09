1d88f1625254255b59ffb154fd3b6d7e50fc9412
Evgeniy Doroshenko <kraspy@yandex.ru>
2024-11-09 13:38:51 +0700

    Refactor: [bot/utils] Add docstrings & refactor code

--- bot/utils/md_utils.py
+++ bot/utils/md_utils.py
@@ -1,32 +1,40 @@
 import markdown
 from bs4 import BeautifulSoup
 
+SUPPORTED_TAGS = [
+    'b',  # жирный текст
+    'strong',  # жирный текст (аналог <b>)
+    'i',  # курсив
+    'em',  # курсив (аналог <i>)
+    'u',  # подчеркнутый текст
+    'ins',  # подчеркнутый текст (аналог <u>)
+    's',  # зачеркнутый текст
+    'strike',  # зачеркнутый текст (устаревший, аналог <s>)
+    'del',  # зачеркнутый текст (аналог <s>)
+    'tg-spoiler',  # спойлер (Telegram)
+    'a',  # ссылка
+    'tg-emoji',  # emoji (Telegram)
+    'code',  # встроенный код
+    'pre',  # блок предформатированного кода
+    'blockquote',  # блок цитирования
+    'blockquote_expandable',  # блок цитирования с возможностью скрыть часть текста
+]
+
 
 def markdown_to_telegram_html(markdown_text: str) -> str:
+    '''Конвертирует Markdown в HTML, совместимый с Telegram.
+
+    Args:
+        markdown_text (str): Текст в формате Markdown.
+
+    Returns:
+        str: HTML-контент, совместимый с Telegram.
+    '''
     html_content = markdown.markdown(markdown_text, extensions=['extra', 'sane_lists'])
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-    def filter_supported_html(html):
-        soup = BeautifulSoup(html, 'html.parser')
-        for tag in soup.find_all(True):  # Ищем все теги
-            if tag.name not in [
-                'b',  # жирный текст
-                'strong',  # жирный текст (аналог <b>)
-                'i',  # курсив
-                'em',  # курсив (аналог <i>)
-                'u',  # подчеркнутый текст
-                'ins',  # подчеркнутый текст (аналог <u>)
-                's',  # зачеркнутый текст
-                'strike',  # зачеркнутый текст (устаревший, аналог <s>)
-                'del',  # зачеркнутый текст (аналог <s>)
-                'tg-spoiler',  # спойлер (Telegram)
-                'a',  # ссылка
-                'tg-emoji',  # emoji (Telegram)
-                'code',  # встроенный код
-                'pre',  # блок предформатированного кода
-                'blockquote',  # блок цитирования
-                'blockquote_expandable',  # блок цитирования с возможностью скрыть часть текста
-            ]:
-                tag.unwrap()
-        return str(soup)
+    for tag in soup.find_all(True):
+        if tag.name not in SUPPORTED_TAGS:
+            tag.unwrap()
 
-    return filter_supported_html(html_content)
+    return str(soup)
--- bot/utils/validation.py
+++ bot/utils/validation.py
@@ -1,31 +1,40 @@
 import re
 
 
-def validate_fullname(fullname: str) -> bool:
-    fullname = fullname.strip()
+def validate_fullname(fullname):
+    '''Проверяет корректность полного имени.
 
+    Args:
+        fullname (str): Полное имя.
+
+    Returns:
+        bool: True, если имя корректно, иначе False.
+    '''
+    fullname = fullname.strip()
     parts = fullname.split()
 
     if len(parts) != 2:
         return False
 
     pattern = re.compile(r'^[А-ЯЁ][а-яё\-]*[а-яё]$')
-    for part in parts:
-        if not pattern.match(part):
-            return False
-    return True
+    return all(pattern.match(part) for part in parts)
 
 
-def validate_phone(phone: str) -> str | None:
+def validate_phone(phone):
+    '''Проверяет и форматирует номер телефона.
+
+    Args:
+        phone (str): Номер телефона.
+
+    Returns:
+        str or None: Форматированный номер, если корректен, иначе None.
+    '''
     digits = re.sub(r'\D', '', phone)
 
-    if digits.startswith('8'):
-        digits = digits[1:]
-    elif digits.startswith('7'):
+    if digits.startswith(('7', '8')):
         digits = digits[1:]
 
     if len(digits) == 10 and digits.startswith('9'):
-        formatted_number = f'7{digits}'
-        return formatted_number
+        return f'7{digits}'
     else:
         return None
